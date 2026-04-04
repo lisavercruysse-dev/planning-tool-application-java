e8c272407fcde0e4c92891147ac5d27dcb11036d
Lisa Vercruysse <lisavercruysse.dev@gmail.com>
2026-04-04 13:20:49 +0200

    Navbar + navigatie.

---
+++ src/main/java/org/sdp/sdp/gui/MainController.java
@@ -0,0 +1,69 @@
+package org.sdp.sdp.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainController {
+
+    private final Map<ToggleButton, String> navButtons = new HashMap<>();
+
+    @FXML
+    public ToggleButton btnGebruikers;
+
+    @FXML
+    public ToggleButton btnLogs;
+
+    @FXML
+    public ToggleButton btnMeldingen;
+
+    @FXML
+    private ToggleGroup navGroup;
+
+    @FXML
+    private StackPane contentPane;
+
+    @FXML
+    private void initialize() {
+        navButtons.put(btnGebruikers, "/org.sdp.sdp/gui/Gebruikers.fxml");
+        navButtons.put(btnLogs, "/org.sdp.sdp/gui/Logs.fxml");
+        navButtons.put(btnMeldingen, "/org.sdp.sdp/gui/meldingen.fxml");
+
+        navGroup.selectedToggleProperty().addListener((obs, oldToggle, newToggle) -> {
+            if (newToggle != null) {
+                Node selected = (Node) newToggle;
+                String path = navButtons.get(selected);
+                if (path != null) {setContent(path);
+                }
+            }
+        });
+
+        setDefaultBtn(btnGebruikers);
+    }
+
+    private void setContent(String fxmlPath) {
+        contentPane.getChildren().clear();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Node newContent = loader.load();
+            contentPane.getChildren().add(newContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setDefaultBtn(ToggleButton defaultBtn) {
+        if (defaultBtn != null) {
+            defaultBtn.setSelected(true);
+        }
+    }
+
+}
