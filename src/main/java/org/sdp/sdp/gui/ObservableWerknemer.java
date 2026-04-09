aad70d3f4d0aeacf6fbafe678d3b4e4e3e961484
PATRICK VAN KERCKHOVE <patrick.vankerckhove@student.hogent.be>
2026-04-09 11:45:26 +0200

    Werknemerslist toegevoegd

---
+++ src/main/java/org/sdp/sdp/gui/ObservableWerknemer.java
@@ -0,0 +1,44 @@
+package org.sdp.sdp.gui;
+
+import domein.Werknemer;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import lombok.Getter;
+
+public class ObservableWerknemer {
+
+    private final StringProperty voornaam;
+    private final StringProperty achternaam;
+    private final StringProperty jobTitel;
+    private final StringProperty email;
+
+    @Getter
+    private final Werknemer werknemer; // koppeling naar domein
+
+    public ObservableWerknemer(Werknemer werknemer) {
+        this.werknemer = werknemer;
+        this.voornaam = new SimpleStringProperty(werknemer.getVoornaam());
+        this.achternaam = new SimpleStringProperty(werknemer.getAchternaam());
+        this.jobTitel = new SimpleStringProperty(werknemer.getJobTitel());
+        this.email = new SimpleStringProperty(werknemer.getEmail());
+
+        // wijzigingen in GUI doorgeven aan domein
+        this.voornaam.addListener((obs, oldVal, newVal) -> werknemer.setVoornaam(newVal));
+        this.achternaam.addListener((obs, oldVal, newVal) -> werknemer.setAchternaam(newVal));
+    }
+
+    public StringProperty firstNameProperty() { return voornaam; }
+    public StringProperty lastNameProperty() { return achternaam; }
+    public StringProperty jobTitelProperty() { return jobTitel; }
+    public StringProperty emailProperty() { return email; }
+
+    public String getFirstName() {
+        return voornaam.get();
+    }
+    public String getLastName() {
+        return achternaam.get();
+    }
+    //public String getJobTitel() { return jobTitel.get(); }
+    //public String getEmail() { return email.get(); }
+}
+
