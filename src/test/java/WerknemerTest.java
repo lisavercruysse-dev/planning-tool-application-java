d0d8ab486be6701eb16f235e5f8abaa5cc6e7d65
Lisa Vercruysse <lisa.vercruysse@student.hogent.be>
2026-04-14 15:44:45 +0200

    kleine aanpassing testen

--- src/test/java/WerknemerTest.java
+++ src/test/java/WerknemerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import repository.GebruikerDao;
 import repository.GenericDao;
 
 import java.util.stream.Stream;
@@ -21,7 +22,7 @@ import static org.mockito.Mockito.verify;
 public class WerknemerTest {
 
     @Mock
-    private GenericDao<Werknemer> werknemerRepo;
+    private GebruikerDao werknemerRepo;
 
     @InjectMocks
     private WerknemerManager werknemerManager;
